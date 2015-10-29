31e8133be95cd1b3f8ea733b954282dcf0736940
Aeowynn <earthsea14@gmail.com>
2015-10-28 18:39:31 -0600

    Merged upstream and master

---
+++ projects/5/intro_to_javascript.js
@@ -0,0 +1,25 @@
+//Introduction to Javascript!
+
+//Print to the console
+console.log('Intro to JS');
+//Variables
+//namespace & globalspace
+x = 100; //globalspace
+//var x = "abc"; //namespace
+//let x = 100;
+//const x = 100;
+
+(function(){
+    function foo () {
+        //var x = 500;
+        console.log(x);
+    }
+    foo();
+})();
+
+//Types
+//Operators
+//Objects
+//Arrays
+//Conditionals
+//Expressions
