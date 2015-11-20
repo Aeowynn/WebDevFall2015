85d6fde62589d74092d60c6c8655a2c191f3c77e
Aeowynn <earthsea14@gmail.com>
2015-11-19 20:14:46 -0700

    Made progress on SmallWorldScore

--- practice/BoardGameApps/assets/js/smallworld_js.js
+++ practice/BoardGameApps/assets/js/smallworld_js.js
@@ -1,3 +1,30 @@
 $(document).readyfunction() {
     //for player in playerlist -> make a new player object for each
-});
+        //also increment the numplayers value
+    //numplayers == turns
+}); 
+
+function Player(name) {
+    this.name = name;
+    var score = 0;
+}
+
+var Kara = new Player("Kara");
+var Josh = new Player("Josh");
+
+var Game = {
+    turns: 0,
+    endingTurn: 10,
+    lastInput: [],
+    add: function(points, player) {
+        player.score += points;
+        lastInput = [player, points];
+    },
+    nextTurn: function() {
+        this.turns += 1;
+    },
+    voidLastInput: function(){
+        this.lastInput[1].score -= lastInput[2];
+    },
+};
+
